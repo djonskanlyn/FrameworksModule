d1c1b6ec4f9fce27a46640e5fdc81af1796e515f
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-06-27 21:25:46 +0100

    walkthrough 5 complete

--- Walkthroughs/django_project/blog/views.py
+++ Walkthroughs/django_project/blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Steve K',
-        'title': 'First Blog Post',
-        'content': 'Content for the first post.',
-        'date_posted': '12/07/2023'
-    },
-
-     {
-        'author': 'Scott K',
-        'title': 'Second Blog Post',
-        'content': 'Content for the second post.',
-        'date_posted': '13/07/2023'
-    }
-] 
-
+from .models import Post
 
 # Create your views here.
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
