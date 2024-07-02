ddff5af0d7b682ae6236feaeab00464fde58a830
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-07-02 21:25:56 +0100

    still to finish 7

--- Walkthroughs/django_project/users/views.py
+++ Walkthroughs/django_project/users/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required #Added import here
 from django.contrib import messages
-from .forms import UserRegisterForm  #Import added
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST) #UserRegisterForm instead of UserCreationForm
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')  
+            messages.success(request, f'Account created successfully, now you can login.')
+            return redirect('login') 
     else:
-        form = UserRegisterForm() #UserRegisterForm instead of UserCreationForm
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required # Added decorator here
+def profile(request): 
+    return render(request, 'users/profile.html')
