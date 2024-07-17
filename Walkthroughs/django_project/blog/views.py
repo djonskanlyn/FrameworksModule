588d14b6918257370c4720dee78ebd04d6877317
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-07-17 22:05:42 +0100

    wlk 10 & tut 6 complete

--- Walkthroughs/django_project/blog/views.py
+++ Walkthroughs/django_project/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -12,22 +12,44 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post  
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
      
-    #Overriding form_valid method 
     def form_valid(self, form):
-        form.instance.author = self.request.user # Set the author on the form
-        return super().form_valid(form) # Validate form by running form_valid method from parent class.
-
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+     
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): 
+    model = Post
+    success_url = "/" # Here we are redirecting the user back to the homepage after deleting a Post successfully
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
