bf91b0ba6c1930b63be2f1d5b0ff61c9f5553caa
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-04 19:05:27 +0100

    react update

--- Tutorials/mysite/polls/admin.py
+++ Tutorials/mysite/polls/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+    ]
+
+admin.site.register(Question,QuestionAdmin)
